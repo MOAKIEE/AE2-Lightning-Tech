348ea66820b0e3606d965717f28a1b74aa55e93f
MOAKIEE <1324227985@qq.com>
2026-05-09 10:08:06 +0800

    Remove lightning collector Jade tooltip

--- src/main/java/com/moakiee/ae2lt/integration/jade/AE2LTJadePlugin.java
+++ src/main/java/com/moakiee/ae2lt/integration/jade/AE2LTJadePlugin.java
@@ -1,9 +1,5 @@
 package com.moakiee.ae2lt.integration.jade;
 
-import com.moakiee.ae2lt.blockentity.LightningCollectorBlockEntity;
-import net.minecraft.world.level.block.Block;
-import snownee.jade.api.IWailaClientRegistration;
-import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
 
@@ -11,14 +7,4 @@ import snownee.jade.api.WailaPlugin;
 public class AE2LTJadePlugin implements IWailaPlugin {
     // Keep AE2LT-owned Jade providers in this package so addon-specific tooltip
     // code stays separate from AE2's own Jade/WTHIT/TOP abstraction layer.
-
-    @Override
-    public void register(IWailaCommonRegistration registration) {
-        registration.registerBlockDataProvider(LightningCollectorJadeProvider.INSTANCE, LightningCollectorBlockEntity.class);
-    }
-
-    @Override
-    public void registerClient(IWailaClientRegistration registration) {
-        registration.registerBlockComponent(LightningCollectorJadeProvider.Client.INSTANCE, Block.class);
-    }
 }
